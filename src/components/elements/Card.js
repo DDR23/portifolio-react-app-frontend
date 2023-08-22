776ec54b89163a2ea759698204ab4b15a2b1f99d
André Campos <roboreserva.andre@gmail.com>
2023-08-22 20:26:12 -0300

    adicionando alterações de eventos e efeitos

--- src/components/elements/Card.js
+++ src/components/elements/Card.js
@@ -1,16 +1,29 @@
 import styles from './Card.module.css'
 import ButtonB from './ButtonB'
+import { useState } from 'react'
 
 function Card({img, title, tech, description, repo, site}){
+    const[info, setInfo] = useState(false)
+
+    function InfoOn(){
+        setInfo(true)
+    }
+
+    function InfoOff(){
+        setInfo(false)
+    }
+
     return(
-        <div className={styles.card}>
-            <a href={site}><img src={img}></img></a>
-            <div>
-                <h3>{title}</h3>
-                <p><strong>Tecnologia: </strong>{tech}</p>
-                <p>{description}</p>
-                <ButtonB text='repositório' link={repo}/>
-            </div>
+        <div onMouseLeave={InfoOff} className={styles.card}>
+            <a onMouseEnter={InfoOn} href={site}><img src={img}></img></a>
+            {info === true &&(
+                <div>
+                    <h3>{title}</h3>
+                    <p><strong>Tecnologia: </strong>{tech}</p>
+                    <p>{description}</p>
+                    <ButtonB text='repositório' link={repo}/>
+                </div>
+            )}
         </div>
     )
 }
